73232ca5658bd0ac144aca5709ec3db9b41c21f7
Richie <richiehopkinson@hotmail.co.uk>
2025-10-02 01:05:10 +0100

    Skeleton layout

--- src/home.js
+++ src/home.js
@@ -1,11 +1,11 @@
-import bannerIMG from './images/food.jpg';
+// import bannerIMG from './images/food.jpg';
 
 export default function loadHome() {
     //Set the nav button background colours.
     document.getElementById("homeButton").style.background='lightgray';
-    document.getElementById("menuButton").style.background='transparent';
-    document.getElementById("aboutButton").style.background='transparent';
-    document.getElementById("contactButton").style.background='transparent';
+    document.getElementById("projectsButton").style.background='transparent';
+    document.getElementById("tasksButton").style.background='transparent';
+    // document.getElementById("contactButton").style.background='transparent';
 
     const content = document.querySelector('#content'); 
     content.innerHTML = '';
@@ -14,21 +14,21 @@ export default function loadHome() {
     menu.innerHTML = '';
 
     const greeting = document.createElement('p');
-    const mission = document.createElement('p2'); 
-    const banner = document.createElement('img'); 
-    const blankLine = document.createElement("p2");
+    // const mission = document.createElement('p2'); 
+    // const banner = document.createElement('img'); 
+    // const blankLine = document.createElement("p2");
 
     greeting.setAttribute("id", "greeting")
 
-    greeting.textContent = 'Welcome to Deli-Shus!'; 
-    mission.innerHTML = "At <strong>Deli-Shus</strong> you'll find something different for the discerning palate, along with some old favourites."; 
-    banner.src = bannerIMG; 
-    banner.alt = 'Food, glorious food';
-    banner.style.maxWidth = '100%';
-    banner.style.height = 'auto';
+    greeting.textContent = 'Home'; 
+    // mission.innerHTML = "At <strong>Deli-Shus</strong> you'll find something different for the discerning palate, along with some old favourites."; 
+    // banner.src = bannerIMG; 
+    // banner.alt = 'Food, glorious food';
+    // banner.style.maxWidth = '100%';
+    // banner.style.height = 'auto';
 
     content.appendChild(greeting); 
-    content.appendChild(banner);
-    content.appendChild(blankLine);
-    content.appendChild(mission);
+    // content.appendChild(banner);
+    // content.appendChild(blankLine);
+    // content.appendChild(mission);
 } 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import "./styles.css";
 
 import loadHome from './home.js';
-import loadMenu from './menu.js';
-import loadAbout from './about.js';
-import loadContact from './contact.js';
+import loadProjects from './projects.js';
+import loadTasks from './tasks.js';
+// import loadContact from './contact.js';
 
 // Always load home page'
 loadHome();
 
 // Other pages. 
 document.getElementById('homeButton').addEventListener('click', loadHome);
-document.getElementById('menuButton').addEventListener('click', loadMenu);
-document.getElementById('aboutButton').addEventListener('click', loadAbout);
-document.getElementById('contactButton').addEventListener('click', loadContact);
+document.getElementById('projectsButton').addEventListener('click', loadProjects);
+document.getElementById('tasksButton').addEventListener('click', loadTasks);
+// document.getElementById('contactButton').addEventListener('click', loadContact);
