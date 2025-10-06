113a83af95e9a97dca53c9b5c01111cb0b246fa4
Richie <richiehopkinson@hotmail.co.uk>
2025-10-06 14:28:56 +0100

    Style and layout changes

--- src/projects.js
+++ src/projects.js
@@ -1,11 +1,20 @@
 export function loadProjects() {
 
     const projectsArray = [
-        { ID: 1, Name: 'General', Description: 'Default project' },
+        { ID: 1, Name: 'Default', Description: 'Default project' },
         { ID: 2, Name: 'Work', Description: 'Work-based tasks' },
         { ID: 3, Name: 'Health and Fitness', Description: 'Health and fitness tasks' },
         { ID: 4, Name: 'Gym', Description: 'Gym tasks, sub-project of Health and Fitness' }
     ]    
+
+    // Sort the array by name
+    projectsArray.sort(function(a, b){
+        let x = a.Name.toLowerCase();
+        let y = b.Name.toLowerCase();
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
+        return 0;
+    }); 
 
     const projects = document.querySelector('#projects');
     projects.innerHTML = '';
