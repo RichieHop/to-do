d9ac87f03b858238e775b4e2e5c4468d9daef264
Richie <richiehopkinson@hotmail.co.uk>
2025-10-10 16:15:37 +0100

    Date formatting

--- src/tasks.js
+++ src/tasks.js
@@ -1,27 +1,30 @@
+import {format} from "date-fns"
+// import { el } from "date-fns/locale";
+
 export function loadTasks() {
 
     const tasksArray = [
-        { ID: 1, Name: 'First task', Description: 'Book service for December', Priority: "Low", Due_Date: 30/10/2025, Project_ID: 6, Completed_Date: null, 
+        { ID: 1, Name: 'First task', Description: 'Book service for December', Priority: "Low", Due_Date: "2025-10-30", Project_ID: 6, Completed_Date: null, 
             Created_Date: 7/10/2025, Sequence_Number: 1 },
-        { ID: 2, Name: 'Task 2', Description: 'Check dash-cams with main dealer', Priority: "Medium", Due_Date: 31/11/2025, Project_ID: 6, Completed_Date: null, 
+        { ID: 2, Name: 'Task 2', Description: 'Check dash-cams with main dealer', Priority: "Medium", Due_Date: "2025-11-31", Project_ID: 6, Completed_Date: null, 
             Created_Date: 7/10/2025, Sequence_Number: 2 },
-        { ID: 3, Name: 'Task 3', Description: 'Wash', Priority: "High", Due_Date: 20/10/2025, Project_ID: 6, Completed_Date: null, 
+        { ID: 3, Name: 'Task 3', Description: 'Wash', Priority: "High", Due_Date: "2025-10-20", Project_ID: 6, Completed_Date: null, 
             Created_Date: 7/10/2025, Sequence_Number: 3 },
-        { ID: 4, Name: 'Task 4', Description: 'Vacuum interior', Priority: "Low", Due_Date: 28/10/2025, Project_ID: 6, Completed_Date: null, 
+        { ID: 4, Name: 'Task 4', Description: 'Vacuum interior', Priority: "Low", Due_Date: "2025-10-28", Project_ID: 6, Completed_Date: null, 
             Created_Date: 7/10/2025, Sequence_Number: 4 },
-        { ID: 5, Name: 'Task 5', Description: 'Clean windows', Priority: "Low", Due_Date: 29/10/2025, Project_ID: 6, Completed_Date: null, 
+        { ID: 5, Name: 'Task 5', Description: 'Clean windows', Priority: "Low", Due_Date: "2025-10-29", Project_ID: 6, Completed_Date: null, 
             Created_Date: 7/10/2025, Sequence_Number: 5 },
-        { ID: 6, Name: 'Task 6', Description: 'Air freshener', Priority: "Low", Due_Date: 30/10/2025, Project_ID: 6, Completed_Date: null, 
+        { ID: 6, Name: 'Task 6', Description: 'Air freshener', Priority: "Low", Due_Date: "2025-10-30", Project_ID: 6, Completed_Date: null, 
             Created_Date: 7/10/2025, Sequence_Number: 6 },
-        { ID: 7, Name: 'Task 7', Description: 'Task 7', Priority: "High", Due_Date: 30/10/2025, Project_ID: 6, Completed_Date: null, 
+        { ID: 7, Name: 'Task 7', Description: 'Task 7', Priority: "High", Due_Date: "2025-10-30", Project_ID: 6, Completed_Date: null, 
             Created_Date: 7/10/2025, Sequence_Number: 7 },
-        { ID: 8, Name: 'Task 8', Description: 'Task 8', Priority: "Low", Due_Date: 30/10/2025, Project_ID: 6, Completed_Date: null, 
+        { ID: 8, Name: 'Task 8', Description: 'Task 8', Priority: "Low", Due_Date: "2025-10-30", Project_ID: 6, Completed_Date: null, 
             Created_Date: 7/10/2025, Sequence_Number: 8 },
-        { ID: 9, Name: 'Task 9', Description: 'Task 9', Priority: "Low", Due_Date: 30/10/2025, Project_ID: 6, Completed_Date: null, 
+        { ID: 9, Name: 'Task 9', Description: 'Task 9', Priority: "Low", Due_Date: "2025-10-30", Project_ID: 6, Completed_Date: null, 
             Created_Date: 7/10/2025, Sequence_Number: 9 },
-        { ID: 10, Name: 'Task 10', Description: 'Task 10', Priority: "Medium", Due_Date: 30/10/2025, Project_ID: 6, Completed_Date: null, 
+        { ID: 10, Name: 'Task 10', Description: 'Task 10', Priority: "Medium", Due_Date: "2025-10-30", Project_ID: 6, Completed_Date: null, 
             Created_Date: 7/10/2025, Sequence_Number: 10 },
-        { ID: 11, Name: 'Task 11', Description: 'Task 11', Priority: "Medium", Due_Date: 30/10/2025, Project_ID: 6, Completed_Date: null, 
+        { ID: 11, Name: 'Task 11', Description: 'Task 11', Priority: "Medium", Due_Date: "2025-10-30", Project_ID: 6, Completed_Date: null, 
             Created_Date: 7/10/2025, Sequence_Number: 11 },
     ]    
 
@@ -37,10 +40,45 @@ export function loadTasks() {
     const tasksContainer = document.querySelector('#tasksContainer');
     tasksContainer.innerHTML = '';
 
+    // Create task headers
+    let taskBody = document.createElement('div');
+    taskBody.classList.add('task');
+    // Set the data index equal to 0, and data project equal to "Headers"
+    taskBody.setAttribute('data-index', 0);
+    taskBody.setAttribute('data-project', `Headers`)
+        
+    // Create the task title header
+    let taskTitle = document.createElement('div');
+    taskTitle.classList.add('task_title');
+    taskTitle.textContent = "Task";
+    taskBody.appendChild(taskTitle);
+
+    // Create the task description header
+    let taskDescription = document.createElement('div');
+    taskDescription.classList.add('task_description');
+    taskDescription.textContent = "Description";
+    taskBody.appendChild(taskDescription);
+
+    // Create the task due date header
+    let taskDueDate = document.createElement('div');
+    taskDueDate.classList.add('task_due_date');
+    taskDueDate.textContent = "Due Date";
+    taskBody.appendChild(taskDueDate);
+
+    // Create the task completed date header
+    let taskCompletedDate = document.createElement('div');
+    taskCompletedDate.classList.add('task_completed_date');
+    taskCompletedDate.textContent = "Completed Date";
+    taskBody.appendChild(taskCompletedDate);
+
+    tasksContainer.appendChild(taskBody);
+
+
+
     for (let i = 0; i < tasksArray.length; ++i) {
 
         // Create the main body of the task
-        const taskBody = document.createElement('div');
+        taskBody = document.createElement('div');
         taskBody.classList.add('task');
         taskBody.classList.add(`priority-${tasksArray[i].Priority}`);
         // Set the data index equal to the array counter, and data project equal to the project name
@@ -48,39 +86,44 @@ export function loadTasks() {
         taskBody.setAttribute('data-project', `${tasksArray[i].Name}`)
             
         // Create the task title
-        const taskTitle = document.createElement('div');
+        taskTitle = document.createElement('div');
         taskTitle.classList.add('task_title');
         taskTitle.textContent = tasksArray[i].Name;
         taskBody.appendChild(taskTitle);
 
         // Create the task description
-        const taskDescription = document.createElement('div');
+        taskDescription = document.createElement('div');
         taskDescription.classList.add('task_description');
         taskDescription.textContent = tasksArray[i].Description;
         taskBody.appendChild(taskDescription);
 
         // Create the task due date, format dd/mm/yyyy.
-        // const taskDate = document.createElement('div');
-        // taskDate.classList.add('task_date');
+        taskDueDate = document.createElement('div');
+        taskDueDate.classList.add('task_due_date');
 
-        // const dateObject = new Date(tasksArray[i].Due_Date);
-        // const dateMonth = format(dateObject, 'MMM');
-        // const dateDay = format(dateObject, 'do');
-        // const dateFormated = `${dateMonth} ${dateDay}`;
+        let dateObject = new Date(tasksArray[i].Due_Date);
+        let dateYear = format(dateObject, 'yyyy');
+        let dateMonth = format(dateObject, 'MM');
+        let dateDay = format(dateObject, 'dd');
+        let dateDMY = `${dateDay}/${dateMonth}/${dateYear}`;
 
-        // taskDate.textContent = dateFormated;
-        // taskDate.textContent = datestring;
-        // taskDate.textContent = tasksArray[i].Due_Date;
+        taskDueDate.textContent = dateDMY;
+        taskBody.appendChild(taskDueDate);
 
-        // let date = new Date(tasksArray[i].Due_Date);
-        // // console.log(date);
-        // let year = Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
-        // let month = Intl.DateTimeFormat('en', { month: 'short' }).format(date);
-        // let day = Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
-        // let formattedDate = (`${day}-${month}-${year}`);
-        // // console.log(formattedDate);
-        // taskDate.textContent = formattedDate;
-        // taskBody.appendChild(taskDate);
+        // Create the task completed date, format dd/mm/yyyy.
+        taskCompletedDate = document.createElement('div');
+        taskCompletedDate.classList.add('task_completed_date');
+
+        taskCompletedDate.textContent = "";
+        if (!dateObject === null) {
+        dateObject = new Date(tasksArray[i].Completed_Date);
+        dateYear = format(dateObject, 'yyyy');
+        dateMonth = format(dateObject, 'MM');
+        dateDay = format(dateObject, 'dd');
+        dateDMY = `${dateDay}/${dateMonth}/${dateYear}`;
+        taskCompletedDate.textContent = dateDMY;
+        }
+        taskBody.appendChild(taskCompletedDate);
 
         tasksContainer.appendChild(taskBody);
 
