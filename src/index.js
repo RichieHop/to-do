f432913530fec564547afaa8e545192a4bc04bde
Richie <richiehopkinson@hotmail.co.uk>
2025-10-14 18:55:07 +0100

    Display tasks for selected project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import "./styles.css";
 
-import { loadProjects, createProject, deleteProject } from './projects.js';
-import { loadTasks, createTask, deleteTask } from './tasks.js';
-
-import displayProjects from './dom_handler.js';
+import { loadProjects, loadTasks, createProject, deleteProject } from './projects.js';
 
 /* Storing user's device details in a variable*/
 let details = navigator.userAgent;
